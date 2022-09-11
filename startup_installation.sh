ba042c01c01448a53d1900657fce1b79777a39ba
Juan Manuel Young Hoyos <36933318+Youngermaster@users.noreply.github.com>
2022-09-10 20:01:35 -0500

    RPM Fusion and Flathub support added

--- startup_installation.sh
+++ startup_installation.sh
@@ -9,6 +9,26 @@ defaultyes=True
 keepcache=True
 " >> /etc/dnf/dnf.conf
 
+# Updating the packages
 sudo dnf update -y
 
-sudo dnf install vim neofetch keepassxc -y
+# Adding RPM fusion
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf groupupdate core
+
+# Install Media Codecs
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf groupupdate sound-and-video -y
+
+# Adding Flathub support
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# General stuff
+sudo dnf install vim neofetch keepassxc gnome-tweaks gnome-extensions-app git cmake htop wget curl nmap ranger -y
+
+# Development stuff
+sudo dnf install make automake gcc gcc-c++ kernel-devel -y
+sudo dnf groupinstall "Development Tools" "Development Libraries" -y
+
+# Multimedia stuff
+sudo dnf install vlc -y
